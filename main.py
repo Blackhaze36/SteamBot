ddc7a221990edc30a42c42b134ebb48f8d5276cf
Brandon Thomas <brandonc.thomas5@gmail.com>
2018-09-24 19:19:24 -0600

    Consolidated commands so they start with !. Cleaned up if else statement

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
-# Steam Bot. Will run commands on PieBox through messages on discord
+# Steam Bot. Will accept commands on discord and run LinuxGSM commands on local host
 import discord
 import subprocess
 
 # create discord client
 client = discord.Client()
 
-# token from https://discordapp.com/developers
+# Token from https://discordapp.com/developers
 TOKEN = 'NDkyNDkyMDkwNTc2NDcwMDE3.DoXixQ.Jore9QdvVTRHoVzJv2JW9JbexHY'
 
-#Dictionary for all our commands. discordCommand/LGSMCommand
-commands = {
-    'css_start': ['/home/steam/LinuxGSM/cssserver', 'start'],
-    'css_stop': ['/home/steam/LinuxGSM/cssserver', 'stop'],
-    'css_restart': ['/home/steam/LinuxGSM/cssserver', 'restart'],
-    'ins_start': ['/home/steam/LinuxGSM/insserver', 'start'],
-    'ins_stop': ['/home/steam/LinuxGSM/insserver', 'stop'],
-    'ins_restart': ['/home/steam/LinuxGSM/insserver', 'restart'],
-    'win_dir': ['dir'] #For testing subprocess on windows
+# Dictionary for LinuxGSM commands. !discord_command: ['LinuxGSM', 'command']
+LGSMCommands = {
+    '!css_start': ['/home/steam/LinuxGSM/cssserver', 'start'],
+    '!css_stop': ['/home/steam/LinuxGSM/cssserver', 'stop'],
+    '!css_restart': ['/home/steam/LinuxGSM/cssserver', 'restart'],
+    '!ins_start': ['/home/steam/LinuxGSM/insserver', 'start'],
+    '!ins_stop': ['/home/steam/LinuxGSM/insserver', 'stop'],
+    '!ins_restart': ['/home/steam/LinuxGSM/insserver', 'restart'],
+    '!win_dir': ['dir'] #For testing subprocess on windows
 }
 
-# ran when bot is ready
+# Dictionary for internal discord commands. !discord_command: internal command
+internalCommands = {
+
+}
+
+# Runs once bot is ready
 @client.event
 async def on_ready():
 	try:
@@ -32,32 +37,41 @@ async def on_ready():
 	except Exception as e:
 		print(e)
 
-# on new message
+# Runs once new message is read
 @client.event
 async def on_message(message):
 
-    # Checks if the message was sent by the bot, if so does nothing.
-    # This prevents the bot from responding to itself
-    if message.author == client.user:
-        return
+    # Turns the message.content into a string
+    discordInput = message.content
 
-    # Used to test the bots responce time
-    elif message.content == "!ping":
-        await client.send_message(message.channel, "Pong!")
+    # If the command starts with '!' and the message author is not the bot, continue.
+    if discordInput[0:1] == '!' and message.author != client.user:
 
-    # Logs outs the bot and stops script
-    elif message.content == '!stop':
-        await client.send_message(message.channel, "Steam Bot shutting down")
-        await client.logout()
+        # If message is a key in LGSMCommands, then run the value through subprocess
+        if message.content in LGSMCommands:
+            await client.send_message(message.channel, "Running LGSM command")
+            subprocess.run(LGSMCommands.get(message.content))
 
-    # Checks if message.content is a key in command and then runs it with subprocess
-    elif message.content in commands:
-        await client.send_message(message.channel, "Running command")
-        subprocess.run(commands.get(message.content))
+        # Prints the LGSMCommands Dictionary to show the available commands
+        elif message.content == '!help':
+            for x in LGSMCommands:
+                await client.send_message(message.channel, x)
 
-    #Catches anything that not a command
+        # "pings" the bot to respond
+        elif message.content == '!ping':
+            await client.send_message(message.channel, "Pong!")
+
+        # Logs out bot and stops script
+        elif message.content == '!stop':
+            await client.send_message(message.channel, "Steam Bot shutting down")
+            await client.logout()
+
+        # If the commands starts with ! but isn't a command, reply with an error
+        else:
+            await client.send_message(message.channel, "Invalid Command")
+
+    # If the message.content doesn't start with '!' or the message author is the bot, do nothing.
     else:
-        await client.send_message(message.channel, "Invalid Command")
-
-# start bot
+        return
+# Starts bot
 client.run(TOKEN)
